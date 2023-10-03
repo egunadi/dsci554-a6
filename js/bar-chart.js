14e87fa0efa5ada4b959f84c4b2fd57d9b8075bb
Eben Gunadi <egunadi@usc.edu>
2023-10-03 12:06:51 -0700

    Update bar chart files

--- js/bar-chart.js
+++ js/bar-chart.js
@@ -1,7 +1,3 @@
-// Sample data for the bar chart
-var barChartData = [10, 20, 15, 25, 30];
-
-// Create the bar chart
 function createBarChart() {
     const margin = { top: 20, right: 20, bottom: 40, left: 60 };
     const width = document.getElementById("bar-chart").offsetWidth - margin.left - margin.right;
@@ -17,7 +13,7 @@ function createBarChart() {
         .attr("transform", `translate(${margin.left},${margin.top})`);
 
     // Your data (you can replace this with your actual data)
-    const data = [{"Year":2020,"Country":"Japan","GDP_per_Capita":4296829629.4645013809},{"Year":2010,"Country":"Japan","GDP_per_Capita":3946207401.6206231117},{"Year":2000,"Country":"Japan","GDP_per_Capita":4222408495.9053416252},{"Year":1990,"Country":"Japan","GDP_per_Capita":3667410400.2171754837},{"Year":1980,"Country":"Japan","GDP_per_Capita":2130820940.9567399025},{"Year":1970,"Country":"Japan","GDP_per_Capita":726063391.5564476252},{"Year":2020,"Country":"Korea","GDP_per_Capita":37433461363.1598510742},{"Year":2010,"Country":"Korea","GDP_per_Capita":27095447155.2909965515},{"Year":2000,"Country":"Korea","GDP_per_Capita":13927207168.9442405701},{"Year":1990,"Country":"Korea","GDP_per_Capita":4545694077.9222793579},{"Year":1980,"Country":"Korea","GDP_per_Capita":1040727759.9002435207},{"Year":1970,"Country":"Korea","GDP_per_Capita":85782268.4315086752}];
+    const data = [{"Year":2020,"Country":"Japan","GDP_per_Capita":4296829629.4645013809},{"Year":2010,"Country":"Japan","GDP_per_Capita":3946207401.6206231117},{"Year":2000,"Country":"Japan","GDP_per_Capita":4222408495.9053416252},{"Year":1990,"Country":"Japan","GDP_per_Capita":3667410400.2171754837},{"Year":1980,"Country":"Japan","GDP_per_Capita":2130820940.9567399025},{"Year":1970,"Country":"Japan","GDP_per_Capita":726063391.5564476252},{"Year":2020,"Country":"Korea","GDP_per_Capita":37433461363.1598510742},{"Year":2010,"Country":"Korea","GDP_per_Capita":27095447155.2909965515},{"Year":2000,"Country":"Korea","GDP_per_Capita":13927207168.9442405701},{"Year":1990,"Country":"Korea","GDP_per_Capita":4545694077.9222793579},{"Year":1980,"Country":"Korea","GDP_per_Capita":1040727759.9002435207},{"Year":1970,"Country":"Korea","GDP_per_Capita":85782268.4315086752},{"Year":2020,"Country":"Indonesia","GDP_per_Capita":56787069733.5082778931},{"Year":2010,"Country":"Indonesia","GDP_per_Capita":28129828509.3586807251},{"Year":2000,"Country":"Indonesia","GDP_per_Capita":6912261876.9677944183},{"Year":1990,"Country":"Indonesia","GDP_per_Capita":1354165319.6524004936},{"Year":1980,"Country":"Indonesia","GDP_per_Capita":358783473.1265080571},{"Year":1970,"Country":"Indonesia","GDP_per_Capita":32872685.1500854902},{"Year":2020,"Country":"China","GDP_per_Capita":71131013.8587102741},{"Year":2010,"Country":"China","GDP_per_Capita":30568286.5045609735},{"Year":2000,"Country":"China","GDP_per_Capita":7932930.452937467},{"Year":1990,"Country":"China","GDP_per_Capita":1635852.5130927574},{"Year":1980,"Country":"China","GDP_per_Capita":466991.9158748083},{"Year":1970,"Country":"China","GDP_per_Capita":277155.5647304499},{"Year":2020,"Country":"Singapore","GDP_per_Capita":80611736.0638619959},{"Year":2010,"Country":"Singapore","GDP_per_Capita":63324179.4952736348},{"Year":2000,"Country":"Singapore","GDP_per_Capita":40860548.2235305831},{"Year":1990,"Country":"Singapore","GDP_per_Capita":23324760.134060882},{"Year":1980,"Country":"Singapore","GDP_per_Capita":10775726.8729623388},{"Year":1970,"Country":"Singapore","GDP_per_Capita":2851494.6181331058}];
 
     // Sort data by Year in ascending order
     data.sort((a, b) => a.Year - b.Year);
@@ -68,14 +64,27 @@ function createBarChart() {
         .attr("class", "group")
         .attr("transform", d => `translate(${xScale(d.Year)},0)`);
 
-    const barWidth = xScale.bandwidth() / 2; // Adjust bar width
+    const countries = Array.from(new Set(data.map(d => d.Country))); // Get unique countries
+    const barWidth = xScale.bandwidth() / countries.length; // Calculate bar width dynamically
 
-    groups.append("rect")
-        .attr("x", d => barWidth * (d.Country === "Japan" ? 0 : 1)) // Adjust x-position
+    groups.selectAll("rect")
+        .data((d) => {
+            const countryData = countries.map(country => {
+                const dataPoint = data.find(item => item.Year === d.Year && item.Country === country);
+                return {
+                    country: country,
+                    GDP_per_Capita: dataPoint ? dataPoint.GDP_per_Capita : 0
+                };
+            });
+            return countryData;
+        })
+        .enter()
+        .append("rect")
+        .attr("x", (d, i) => i * barWidth) // Adjust x-position based on index
         .attr("y", d => yScale(d.GDP_per_Capita))
         .attr("width", barWidth)
         .attr("height", d => height - yScale(d.GDP_per_Capita))
-        .attr("fill", d => (d.Country === "Japan") ? "steelblue" : "orange");
+        .attr("fill", d => (d.country === "Japan") ? "steelblue" : "orange");
 
     // Add legend (you can customize this)
     const legend = svg.append("g")
--- py/bar-chart.py
+++ py/bar-chart.py
@@ -13,10 +13,28 @@ def export_gdp_per_capita():
     population_df_korea = population_df[(population_df['Country or Area'] == 'Republic of Korea') & 
                                         (population_df['Variant'] == 'Medium')]
     
+    population_df_indonesia = population_df[(population_df['Country or Area'] == 'Indonesia') & 
+                                        (population_df['Variant'] == 'Medium')]
+
+    population_df_china = population_df[(population_df['Country or Area'] == 'China') & 
+                                        (population_df['Variant'] == 'Medium')]
+
+    population_df_singapore = population_df[(population_df['Country or Area'] == 'Singapore') & 
+                                        (population_df['Variant'] == 'Medium')]
+    
     gdp_df_japan = gdp_df[(gdp_df['Country or Area'] == 'Japan') & 
                             (gdp_df['Item'] == 'Gross Domestic Product (GDP)')]
     
     gdp_df_korea = gdp_df[(gdp_df['Country or Area'] == 'Republic of Korea') & 
+                            (gdp_df['Item'] == 'Gross Domestic Product (GDP)')]
+    
+    gdp_df_indonesia = gdp_df[(gdp_df['Country or Area'] == 'Indonesia') & 
+                            (gdp_df['Item'] == 'Gross Domestic Product (GDP)')]
+    
+    gdp_df_china = gdp_df[(gdp_df['Country or Area'] == 'China (mainland)') & 
+                            (gdp_df['Item'] == 'Gross Domestic Product (GDP)')]
+    
+    gdp_df_singapore = gdp_df[(gdp_df['Country or Area'] == 'Singapore') & 
                             (gdp_df['Item'] == 'Gross Domestic Product (GDP)')]
     
     population_df_japan = population_df_japan[['Year(s)', 'Value']]
@@ -25,7 +43,21 @@ def export_gdp_per_capita():
     population_df_korea = population_df_korea[['Year(s)', 'Value']]
     population_df_korea = population_df_korea.rename(columns={'Value': 'Korea'})
     
-    population_df = population_df_japan.merge(population_df_korea, on='Year(s)')
+    population_df_indonesia = population_df_indonesia[['Year(s)', 'Value']]
+    population_df_indonesia = population_df_indonesia.rename(columns={'Value': 'Indonesia'})
+    
+    population_df_china = population_df_china[['Year(s)', 'Value']]
+    population_df_china = population_df_china.rename(columns={'Value': 'China'})
+    
+    population_df_singapore = population_df_singapore[['Year(s)', 'Value']]
+    population_df_singapore = population_df_singapore.rename(columns={'Value': 'Singapore'})
+    
+    population_df = population_df_japan \
+                            .merge(population_df_korea, on='Year(s)') \
+                            .merge(population_df_indonesia, on='Year(s)') \
+                            .merge(population_df_china, on='Year(s)') \
+                            .merge(population_df_singapore, on='Year(s)')
+                            
     population_df = population_df.rename(columns={'Year(s)': 'Year'})
     
     gdp_df_japan = gdp_df_japan[['Year', 'Value']]
@@ -34,12 +66,29 @@ def export_gdp_per_capita():
     gdp_df_korea = gdp_df_korea[['Year', 'Value']]
     gdp_df_korea = gdp_df_korea.rename(columns={'Value': 'Korea'})
     
-    gdp_df = gdp_df_japan.merge(gdp_df_korea, on='Year')
+    gdp_df_indonesia = gdp_df_indonesia[['Year', 'Value']]
+    gdp_df_indonesia = gdp_df_indonesia.rename(columns={'Value': 'Indonesia'})
+        
+    gdp_df_china = gdp_df_china[['Year', 'Value']]
+    gdp_df_china = gdp_df_china.rename(columns={'Value': 'China'})
+        
+    gdp_df_singapore = gdp_df_singapore[['Year', 'Value']]
+    gdp_df_singapore = gdp_df_singapore.rename(columns={'Value': 'Singapore'})
     
-    population_df_unpivot = pd.melt(population_df, id_vars='Year', value_vars=['Japan', 'Korea'])
+    gdp_df = gdp_df_japan \
+                .merge(gdp_df_korea, on='Year') \
+                .merge(gdp_df_indonesia, on='Year') \
+                .merge(gdp_df_china, on='Year') \
+                .merge(gdp_df_singapore, on='Year')
+    
+    population_df_unpivot = pd.melt(population_df, 
+                                    id_vars='Year', 
+                                    value_vars=['Japan', 'Korea', 'Indonesia', 'China', 'Singapore'])
     population_df_unpivot = population_df_unpivot.rename(columns={'variable': 'Country', 'value': 'Population'})
     
-    gdp_df_unpivot = pd.melt(gdp_df, id_vars='Year', value_vars=['Japan', 'Korea'])
+    gdp_df_unpivot = pd.melt(gdp_df, 
+                             id_vars='Year', 
+                             value_vars=['Japan', 'Korea', 'Indonesia', 'China', 'Singapore'])
     gdp_df_unpivot = gdp_df_unpivot.rename(columns={'variable': 'Country', 'value': 'GDP'})
     
     combined_df = population_df_unpivot.merge(gdp_df_unpivot, on=['Year','Country'])
